9cd6739b13d8d376b089aac6f0ee8fb0b35a3257
Iliya Zhechev <iliya.zhechev@gmail.com>
2024-02-24 10:51:58 +0200

    Linear in dim

--- nca/utils.py
+++ nca/utils.py
@@ -13,3 +13,19 @@ class Reshape(nn.Module):
 
     def forward(self, x):
         return x.reshape(-1, *self.shape)
+
+
+class LinerInDim(nn.Module):
+    def __init__(self, in_size, out_size, dim=-1):
+        super().__init__()
+        self.dim = dim
+        self.linear = nn.Linear(in_size, out_size)
+
+    def forward(self, x):
+        x_dims = list(range(len(x.shape)))
+        x_dims[-1], x_dims[self.dim] = x_dims[self.dim], x_dims[-1]
+        x = x.permute(*x_dims)
+        x = self.linear(x)
+        x = x.permute(*x_dims)
+
+        return x
