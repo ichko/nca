3bf5dc68eb8c9c6f3b4813dbb826c1c29e0b85b9
Iliya Zhechev <iliya.zhechev@gmail.com>
2024-02-25 13:17:45 +0200

    working stream

--- nca/stream.py
+++ nca/stream.py
@@ -1,23 +1,30 @@
-from flask import Flask, render_template, Response
+from flask import Flask, Response
 import cv2
 import numpy as np
 import multiprocessing as mp
 import threading
 import socket
 from contextlib import closing
+from flask import request
+from werkzeug.serving import make_server
 
 
-def find_free_port():
+def _find_free_port():
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
 
-class VideoStream:
-    def __init__(self, frames_generator) -> None:
-        self.frames_generator = frames_generator
+class VideoStream(threading.Thread):
+    def __init__(self) -> None:
+        threading.Thread.__init__(self)
         self.app = Flask(__name__)
+        self.port = _find_free_port()
+        self.server = make_server("0.0.0.0", self.port, self.app)
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+        self.shutdown = False
 
         @self.app.route("/video_feed")
         def video_feed():
@@ -25,23 +32,23 @@ class VideoStream:
                 self._gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
             )
 
-        self.server = threading.Thread(target=self._run)
-
-    def start(self):
-        # mp.set_start_method("fork")
-        # self.server = mp.Process(target=self._run)
-        self.server.start()
+    def run(self):
+        self.server.serve_forever()
 
     def stop(self):
-        self.server.terminate()
+        self.shutdown = True
+        self.server.shutdown()
+
+    @property
+    def url(self):
+        return f"http://localhost:{self.port}/video_feed"
+
+    def _repr_html_(self):
+        return f"<img src='{self.url}' />"
 
     def _gen_frames(self):
-        while True:
+        while not self.shutdown:
             frame = np.random.randint(0, 255, (500, 500, 3))
-            ret, buffer = cv2.imencode(".png", frame)
+            ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
-            yield (b"--frame\r\n" b"Content-Type: image/png\r\n\r\n" + frame + b"\r\n")
-
-    def _run(self):
-        self.port = find_free_port()
-        self.app.run(debug=True, port=self.port)
+            yield (b"--frame\r\n" b"Content-Type: image/jpg\r\n\r\n" + frame + b"\r\n")
